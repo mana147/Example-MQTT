dee915a40439859cb737b1c0b8bcffec9a462c7f
mana147 <mana147@github.com>
2019-11-04 15:47:31 +0700

    update : test thu nghiem ket noi server broker MQTT tren esp8266, done

--- MQTT_for_Esp8266_Arduino/README.md
+++ MQTT_for_Esp8266_Arduino/README.md
@@ -1,1 +1,27 @@
-code MQTT for esp8266 platform Arduino
+# Code MQTT client for Esp8266 platform Arduino
+
+note : server online and test tools on the client
+
+- Server online test  : server itc team 
+    - Broker address : itc-server.dynu.net
+    - Broker port : 8883
+    - Client ID : dfea65c5313c494c80f9aa9336c2d0dd
+    - Enable SSL/TLS : CA signed server certificate 
+    - Protocol : TLS v1.2
+    - User name : user_mobile
+    - Password : 123456
+
+- Server online test  : server broker Cloud MQTT 
+    - Broker address : 	tailor.cloudmqtt.com
+    - Broker port :
+        - port not SSL : 18989
+        - port over SSL : 28989
+        - port socket TSL only : 38989
+    - User name : jwactjqv
+    - Password : OtVca_zVKZO8
+    - Client ID : name divice 
+    - Enable SSL/TLS : CA signed server certificate 
+    - Protocol : TLS v1.2
+    - fingerprint  "39 DF B6 55 54 1D A4 53 12 70 65 DC 43 11 53 DA 3B A3 6E A8" .
+
+
--- MQTT_for_Esp8266_Arduino/main.ino
+++ MQTT_for_Esp8266_Arduino/main.ino
@@ -11,37 +11,110 @@
 
 #include "Include\main_inc.h"
 
-/*----------------------------------------------------------------------------*/
+/************************* WiFi Access Point *********************************/
 
-#define MQTT_SERVER      "mqtt.flespi.io"
-#define MQTT_SERVERPORT  8883
-#define FLESPI_CERT_FINGERPRINT "3B BC 95 33 E5 AB C1 1C C8 FC 37 57 F2 94 2C 43 8E 3B 66 F3"
-#define DEST_TOPIC       "ESP8266/test"
-#define FLESPI_TOKEN    "FlespiToken REPLACE_WITH_YOUR_FLESPI_TOKEN"
+#define WLAN_SSID       "FPT"
 
-const char* ID = "FPT";
-const char*  PASS =  "hpfd23115";
+#define WLAN_PASS       "hpfd23115"
 
-/********************* Global connection instances **************************/
+/************************* SERVER Broker config *********************************/
+
+const char *MQTT_SERVER = "tailor.cloudmqtt.com" ;
+
+const uint16_t MQTT_SERVERPORT = 28989 ;
+
+const char *USERNAME = "jwactjqv";
+
+const char *PASSWORD = "zM6h5Iiaa2bI";
+
+const char * CID = "ESP8266-client-id-12345";
+
+#define fingerprint  "39 DF B6 55 54 1D A4 53 12 70 65 DC 43 11 53 DA 3B A3 6E A8"
+
+#define DEST_TOPIC    "ESP8266/test"
+
+#define FLESPI_TOKEN  "FlespiToken REPLACE_WITH_YOUR_FLESPI_TOKEN"
+
+/*********************** Client config  ******************/
+
 // WiFiFlientSecure for SSL/TLS support
 WiFiClientSecure client;
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, FLESPI_TOKEN, "");
-// Setup a feed 'flespi' to publish messages to flespi MQTT broker.
-Adafruit_MQTT_Publish flespi = Adafruit_MQTT_Publish(&mqtt, DEST_TOPIC);
 
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt ( &client , MQTT_SERVER , MQTT_SERVERPORT , CID , USERNAME,  PASSWORD );
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'test' for publishing.
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+
+Adafruit_MQTT_Publish test = Adafruit_MQTT_Publish( &mqtt, USERNAME , 0 );
+
+/****************************** Function ***************************************/
+
+void MQTT_connect() 
+{
+   mqtt.connected() ? 
+    Serial.println ("/ MQTT connected ") : 
+    Serial.println ("/ MQTT not connected ") ;
+
+  unsigned char ret;
+  unsigned char retries = 3;
+
+  while ((ret = mqtt.connect()) != 0) 
+  { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) 
+    {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+
+}
+
+/****************************** SETUP ***************************************/
 
 void setup()  
 {
   /* Function Config_Wifi */
-  Config_Wifi (ID , PASS);
+
+  Config_Wifi (WLAN_SSID , WLAN_PASS);
+
   /*----------------------*/
 
-  // connect to server check the fingerprint of flespi.io's SSL cert (from Adafruit MQTTS example)
-  verify_fingerprint(client);
+  client.setFingerprint(fingerprint) ? 
+    Serial.println("/ Finger ok") : 
+    Serial.println("/ Finger not ok") ;
 }
+/****************************** LOOP ***************************************/
+
+uint32_t x = 0;
 
 void loop() 
 {
-  // code
+  MQTT_connect();
+
+  // Now we can publish stuff!
+
+  Serial.print(x);
+
+  Serial.print(" to test feed...");
+
+  if (! test.publish(x++))  {
+    Serial.println(" > Failed");
+  } else {
+    Serial.println(" > OK!");
+  }
+ 
+  // wait a couple seconds to avoid rate limit
+  delay(2000);
+
 }
+
+/****************************** END ***************************************/
